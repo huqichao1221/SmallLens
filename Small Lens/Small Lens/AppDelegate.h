411c60a42fd6dd0174d088b15a929e4d4c5d84e6
JM Teo <753331342@qq.com>
2015-08-10 15:08:54 +0800

    创建工程
    开始创建工程

---
+++ Small Lens/Small Lens/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Small Lens
+//
+//  Created by zjm on 15-8-10.
+//  Copyright (c) 2015年 zjm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Small Lens/Small Lens/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Small Lens
+//
+//  Created by zjm on 15-8-10.
+//  Copyright (c) 2015年 zjm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
